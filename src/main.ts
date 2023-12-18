cc9225126f287776ab7a0e787a6e00b6a8f558a6
Aleksander <76603765+Alex99like@users.noreply.github.com>
2023-12-18 16:51:57 +0300

    feat: add test

--- src/main.ts
+++ src/main.ts
@@ -11,47 +11,12 @@ const numCPUs = cpus().length;
 const workers = {};
 
 async function bootstrap() {
-  const getWorkerIndex = (ip: string) => {
-    return farmhash.fingerprint32(ip) % numCPUs;
-  };
+  const app = await NestFactory.create(AppModule);
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
 
-  if (clusterModule.isPrimary) {
-    console.log(`
-             Master server started,proccess.pid:${process.pid},
-             number of cpus: ${numCPUs}
-     `);
-    for (let i = 0; i < numCPUs; i++) {
-      workers[i] = clusterModule.fork();
-      workers[i].on('exit', (worker, code, signal) => {
-        console.log(`
-                      Worker with code: ${code} and    
-                      signal: ${signal} is   Restarting...
-             `);
-        workers[i] = clusterModule.fork();
-      });
-    }
-    net
-      .createServer({ pauseOnConnect: true }, (connection) => {
-        const workerIndex = getWorkerIndex(connection.remoteAddress);
-        workers[workerIndex].send('sticky-session:connection', connection);
-      })
-      .listen(3000);
-  } else {
-    const app = await NestFactory.create(AppModule);
-    console.log(`
-       Worker server started, process.pid: ${process.pid}
-    `);
-    const server = await app.listen(0);
-    process.on('message', (message, connection: any) => {
-      if (message !== 'sticky-session:connection') {
-        return;
-      }
-      server.emit('connection', connection);
-      connection.resume();
-    });
-    const redisIoAdapter = new RedisIoAdapter(app);
-    await redisIoAdapter.connectToRedis();
-    app.useWebSocketAdapter(redisIoAdapter);
-  }
+  app.useWebSocketAdapter(redisIoAdapter);
+
+  await app.listen(3000);
 }
 bootstrap();
